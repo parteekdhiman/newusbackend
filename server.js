af3a4cf76cfc2d22e0fc5e8f1e001c2811f65ac8
newusdharamshala <newusdshala@gmail.com>
2025-05-20 14:12:24 +0530

    add cors

--- server.js
+++ server.js
@@ -5,7 +5,22 @@ const app = express();
 const cors = require("cors");
 const router = require("./controllers/support");
 app.use(express.json());
-app.use(cors({ origin: "http://localhost:5173" }));
+
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://newustest.netlify.app"
+];
+
+
+app.use(cors({
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  }
+}));
 app.use("/enquri", enquri);
 app.use("/support-form", router);
 app.listen(process.env.PORT || 3000, () => {
