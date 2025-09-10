8befe253fc8d2a0482ca42fdb7433509c4d3f53f
parteekdhiman <pkdhiman414@gmail.com>
2025-09-10 15:02:17 +0530

    handel cors

--- api/index.js
+++ api/index.js
@@ -12,7 +12,7 @@ const app = express();
 
 // Middleware
 const allowedOrigins = [
-  'https://newus.in/',
+  'https://newus.in',
   'https://newus-tau.vercel.app',
   'http://localhost:5173',
   'http://localhost:3000'
@@ -27,14 +27,21 @@ app.use(cors({
   origin: function (origin, callback) {
     // Allow requests with no origin (like mobile apps or curl requests)
     if (!origin) return callback(null, true);
-    
-    if (allowedOrigins.indexOf(origin) !== -1) {
+
+    // Normalize possible trailing slash on origin
+    const normalizedOrigin = origin?.replace(/\/$/, '');
+    const isAllowed = allowedOrigins.includes(normalizedOrigin);
+
+    if (isAllowed) {
       callback(null, true);
     } else {
       callback(new Error('Not allowed by CORS'));
     }
   },
-  credentials: true 
+  methods: ['GET', 'POST', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,
+  optionsSuccessStatus: 204
 }));
 app.use(express.json());
 
